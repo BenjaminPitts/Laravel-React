eb7976c68096a69a99fcea520f4d14a5b561b378
Matt Huntington <matt.huntington@gmail.com>
2020-10-09 17:04:55 -0400

    removing auth route

--- routes/api.php
+++ routes/api.php
@@ -14,10 +14,6 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:api')->get('/user', function (Request $request) {
-    return $request->user();
-});
-
 Route::get('people', function () {
     $people = DB::select('SELECT * FROM people');
     return $people;
